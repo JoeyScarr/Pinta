b004df58bff24893baaa3005b97f0a9823d5a9af
Scott Olson <scott@scott-olson.org>
2013-08-09 14:17:12 -0600

    Refactor logging.

--- Pinta.Core/Logger.cs
+++ Pinta.Core/Logger.cs
@@ -9,11 +9,12 @@ namespace Pinta.Core
 
         public static void Log (string format, params object[] args)
         {
-            Console.WriteLine ("{0}: {1}", DateTime.Now, string.Format (format, args));
+            string output = DateTime.Now + ": " + string.Format(format, args);
+            Console.WriteLine (output);
 
             using (var log = new StreamWriter (log_file, true))
             {
-                log.WriteLine ("{0}: {1}", DateTime.Now, string.Format (format, args));
+                log.WriteLine (output);
             }
         }
     }
